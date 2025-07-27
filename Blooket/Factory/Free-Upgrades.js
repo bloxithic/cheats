7edd294f8bde826f84aeeaa5a051b86cfd05a422
Static <bloxithic@gmail.com>
2025-07-27 14:36:03 +0700

    Update Free-Upgrades.js

--- Blooket/Factory/Free-Upgrades.js
+++ Blooket/Factory/Free-Upgrades.js
@@ -1,47 +1,39 @@
-/**
- * @license AGPL-3.0
- * Website Cheats
- * 
- * This script is a modified version of Blooket Cheats by 05Konz.
- * Original source: https://github.com/Blooket-Council/Blooket-Cheats
- * 
- * Modifications made by bloxithic
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 (() => {
     const prices = [0, 0, 0, 0];
-    const getStateNode = () => {
-        const recursiveSearch = (node = document.querySelector("body>div")) => {
-            const reactObj = Object.values(node)[1];
-            return reactObj?.children?.[0]?._owner?.stateNode ? node : recursiveSearch(node.querySelector(":scope>div"));
-        };
-        const root = recursiveSearch();
-        return Object.values(root)[1].children[0]._owner.stateNode;
-    };
 
-    const cheat = () => {
-        const stateNode = getStateNode();
-        if (!stateNode?.state?.blooks) return;
+    function getStateNode() {
+        const root = Object.values(document.querySelector('#app > div > div'))?.find(x => x?.children?.[0]?._owner?.stateNode);
+        return root?.children?.[0]?._owner?.stateNode;
+    }
+
+    function updatePrices(stateNode) {
         stateNode.setState({
             blooks: stateNode.state.blooks.map(blook => {
                 blook.price = prices;
                 return blook;
             })
         });
-    };
+    }
 
-    cheat();
+    const interval = setInterval(() => {
+        try {
+            const stateNode = getStateNode();
+            if (!stateNode?.state?.blooks) return;
+
+            updatePrices(stateNode);
+
+            const originalSetState = stateNode.setState.bind(stateNode);
+            stateNode.setState = (newState, ...rest) => {
+                if (newState?.blooks) {
+                    newState.blooks = newState.blooks.map(blook => {
+                        blook.price = prices;
+                        return blook;
+                    });
+                }
+                return originalSetState(newState, ...rest);
+            };
+
+            clearInterval(interval);
+        } catch {}
+    }, 100);
 })();
