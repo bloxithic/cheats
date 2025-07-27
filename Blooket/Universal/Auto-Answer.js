949e109910c37ddbebca21d274d27b60ce2d5cec
Static <bloxithic@gmail.com>
2025-07-27 10:12:17 +0700

    Update Auto-Answer.js

--- Blooket/Universal/Auto-Answer.js
+++ Blooket/Universal/Auto-Answer.js
@@ -2,8 +2,13 @@ let interval = setInterval(() => {
   try {
     const stateNode = Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode;
     const question = stateNode.state.question;
-    if (!question) throw new Error("No question found.");
     
+    if (!question) {
+      alert("Failed to auto answer questions.");
+      clearInterval(interval);
+      return;
+    }
+
     if (question.qType === "typing") {
       stateNode.sendAnswer?.(question.answers[0]);
     } else {
@@ -11,8 +16,8 @@ let interval = setInterval(() => {
       const correctIndex = question.answers.findIndex(ans => question.correctAnswers.includes(ans));
       answers[correctIndex]?.click();
     }
-  } catch (e) {
-    clearInterval(interval);
+  } catch {
     alert("Failed to auto answer questions.");
+    clearInterval(interval);
   }
 }, 100);
