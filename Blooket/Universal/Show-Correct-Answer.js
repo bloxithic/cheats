c9285f4c1bff22283350fd606ce0b53779c96a46
Static <bloxithic@gmail.com>
2025-07-27 11:00:52 +0700

    Update Show-Correct-Answer.js

--- Blooket/Universal/Show-Correct-Answer.js
+++ Blooket/Universal/Show-Correct-Answer.js
@@ -7,11 +7,17 @@
       question.answers.forEach((ans, i) => {
         const isCorrect = question.correctAnswers.includes(ans);
         const el = document.querySelectorAll("[class*='answersHolder'] > div")[i];
-        if (el) el.children[0].style.backgroundColor = isCorrect ? "rgb(0, 207, 119)" : "rgb(196, 58, 53)";
+        if (el) {
+          const box = el.children[0];
+          box.style.backgroundColor = isCorrect ? "rgb(0, 207, 119)" : "rgb(196, 58, 53)";
+          box.style.opacity = isCorrect ? "1" : "0.5";
+          box.style.pointerEvents = isCorrect ? "auto" : "none";
+          box.style.transition = isCorrect ? "" : "none";
+        }
       });
     } catch (err) {
       clearInterval(loop);
       alert("Failed to show correct answers.");
     }
   }, 100);
-})();
+})()
