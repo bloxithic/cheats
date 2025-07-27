b9807229dbfb1805f29bae16341e4114264c00f5
Static <bloxithic@gmail.com>
2025-07-27 11:23:35 +0700

    Update Auto-Answer.js

--- Blooket/Universal/Auto-Answer.js
+++ Blooket/Universal/Auto-Answer.js
@@ -1,3 +1,26 @@
+/**
+ * @license AGPL-3.0
+ * Modified Blooket Cheats
+ * 
+ * This is a modified version of Blooket Cheats by 05Konz.
+ * Original source: https://github.com/Blooket-Council/Blooket-Cheats
+ * 
+ * Modifications made by bloxithic
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 let interval = setInterval(() => {
   try {
     const stateNode = Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode;
