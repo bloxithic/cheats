60ad54ab02bed587f3d2e3185263f3e573a423e8
Static <bloxithic@gmail.com>
2025-07-27 13:19:32 +0700

    Update Correct-Answer-Rewriter.js

--- Blooket/Universal/Correct-Answer-Rewriter.js
+++ Blooket/Universal/Correct-Answer-Rewriter.js
@@ -1,17 +1,17 @@
-/**
- * Portions of this file are Copyright (c) 2023 TheCinnamonToastCrunch
- * Licensed under the BSD 3-Clause License
- * 
- * See the NOTICE file or https://opensource.org/licenses/BSD-3-Clause for details.
- */
+(() => {
+  const interval = setInterval(() => {
+    try {
+      const reactRoot = Object.values(document.querySelector('#app > div > div')).find(v => v?.children?.[0]?._owner?.stateNode);
+      const stateNode = reactRoot?.children?.[0]?._owner?.stateNode;
+      const question = stateNode?.state?.question;
 
-const interval = setInterval(() => {
-  try {
-    const root = Object.values(document.querySelector('#app > div > div'))[1];
-    const gameState = root.children[0]._owner.stateNode.state;
-    gameState.question.correctAnswers = gameState.question.answers;
-  } catch (err) {
-    clearInterval(interval);
-    alert("Failed to rewrite correct answers.");
-  }
-}, 100);
+      if (!question?.answers) return;
+
+      question.correctAnswers = [...question.answers];
+      stateNode.forceUpdate?.();
+    } catch {
+      clearInterval(interval);
+      alert("Failed to rewrite correct answers");
+    }
+  }, 100);
+})();
