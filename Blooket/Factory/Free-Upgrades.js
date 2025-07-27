65b717bf815628481815a960494843dfa28dad87
Static <bloxithic@gmail.com>
2025-07-27 14:14:52 +0700

    Update Free-Upgrades.js

--- Blooket/Factory/Free-Upgrades.js
+++ Blooket/Factory/Free-Upgrades.js
@@ -1,7 +1,12 @@
 /**
  * @license AGPL-3.0
- * Blooket Cheats
- * Copyright (C) 2023-present 05Konz
+ * Website Cheats
+ * 
+ * This script is a modified version of Blooket Cheats by 05Konz.
+ * Original source: https://github.com/Blooket-Council/Blooket-Cheats
+ * 
+ * Modifications made by bloxithic
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
@@ -13,9 +18,7 @@
  * GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Source: https://github.com/Blooket-Council/Blooket-Cheats 05konz994@gmail.com
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 (() => {
