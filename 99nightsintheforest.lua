90c2ec69117e1572777406188293c7fa687df33e
Static <bloxithic@gmail.com>
2025-09-05 21:56:24 +0700

    Update 99nightsintheforest.lua

--- 99nightsintheforest.lua
+++ 99nightsintheforest.lua
@@ -1,12 +1,5 @@
 local KillAura, KillAuraDistance
 
-task.spawn(function()
-    while task.wait(0.1) do
-        if KillAura then
-            
-    end
-end)
-
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local Window = WindUI:CreateWindow({
@@ -35,16 +28,21 @@ local KillAuraToggle = MainTab:Toggle({
     end
 })
 
-local Input = Tab:Input({
-    Title = "Input",
-    Desc = "Input Description",
-    Value = "Default value",
+local KillAuraDistanceInput = MainTab:Input({
+    Title = "Kill Aura Distance",
+    Value = "0",
     InputIcon = "bird",
-    Type = "Input", -- or "Textarea"
-    Placeholder = "Enter text...",
     Callback = function(input)
         if tonumber(input) then
             KillAuraDistance = input
-                e
+        else
+            WindUI:Notify({
+                Title = "Noxware",
+                Content = "Please enter a valid number.",
+                Duration = 3, -- 3 seconds
+                Icon = "ban",
+            })
+        end
+            
     end
 })
