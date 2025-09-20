3f5ef7cbb8934fee093dade62a621d30afd043b6
Static <bloxithic@gmail.com>
2025-09-20 13:18:17 +0700

    Update 99nightsintheforest.lua

--- 99nightsintheforest.lua
+++ 99nightsintheforest.lua
@@ -1,17 +1,22 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local ply = game:GetService("Players")
+local runservice = game:GetService("RunService")
+local uis = game:GetService("UserInputService")
 local replicated = game:GetService("ReplicatedStorage")
 local eventsFolder = replicated:WaitForChild("RemoteEvents")
 
 local plr = ply.LocalPlayer
+local inv = plr:WaitForChild("Inventory")
 local char = plr.Character or plr.CharacterAdded:Wait()
 local hum = char:WaitForChild("Humanoid")
 local hrp = char:WaitForChild("HumanoidRootPart")
 
-local KillAura = false
-local KillAuraDistance = 50
-local bring, bringpos, tool, part
+local KillAura, bring, bringpos, weapon, speed, jump, speedConnection, infJumpConnection, jumpConnection
+local KillAuraDistance, jump = 50
+local speed = 16
+
+hum.UseJumpPower = true
 
 local toolsDamageIDs = {
     ["Old Axe"] = "1_8982038982",
@@ -32,27 +37,27 @@ end
 task.spawn(function()
 	while task.wait() do
 		if KillAura then
-			tool = char:FindFirstChildOfClass("Tool")
-			if tool and toolsDamageIDs[tool.Name]  then
-				for _, v in ipairs(workspace.Characters:GetChildren()) do
-	                if v:IsA("Model") and v.PrimaryPart then
-	                    if (v.PrimaryPart.Position - hrp.Position).Magnitude <= KillAuraDistance then
-	                        pcall(function()
-	                            eventsFolder.ToolDamageObject:InvokeServer(
-	                                v,
-	                                tool,
-	                                toolsDamageIDs[tool.Name],
-	                                hrp.CFrame
-	                            )
-	                        end)
-	                    end
-	                end
-	            end
+			for _, weapon in ipairs(inv:GetChildren()) do
+				if toolsDamageIDs[weapon.Name] then
+					for _, mob in ipairs(workspace.Characters:GetChildren()) do
+						if mob:IsA("Model") and mob.PrimaryPart then
+							if (mob.PrimaryPart.Position - hrp.Position).Magnitude <= KillAuraDistance then
+								pcall(function()
+									eventsFolder.ToolDamageObject:InvokeServer(
+										mob,
+										weapon,
+										toolsDamageIDs[weapon.Name],
+										hrp.CFrame
+									)
+								end)
+							end
+						end
+					end
+				end
 			end
 		end
 	end
-end)
-			
+end)	
 
 local Window = WindUI:CreateWindow({
     Title = "Noxware",
@@ -66,6 +71,11 @@ local MainTab = Window:Tab({
     Icon = "house",
 })
 
+local PlayerTab = Window:Tab({
+    Title = "Player",
+    Icon = "user",
+})
+
 local BringTab = Window:Tab({
     Title = "Bring",
     Icon = "package",
@@ -76,21 +86,20 @@ local KillAuraSection = MainTab:Section({
     TextXAlignment = "Left",
 })
 
+local PlayerSection = MainTab:Section({ 
+    Title = "Player",
+    TextXAlignment = "Left",
+})
+
 local KillAuraToggle = MainTab:Toggle({
     Title = "Enable Kill Aura",
-    Icon = "sword",
-    Default = false,
     Callback = function(state)
         KillAura = state
     end
 })
 
 local KillAuraDistanceSlider = MainTab:Slider({
-    Title = "Slider",
-    
-    -- To make float number supported, 
-    -- make the Step a float number.
-    -- example: Step = 0.1
+    Title = "Distance",
     Step = 1,
     
     Value = {
@@ -99,7 +108,79 @@ local KillAuraDistanceSlider = MainTab:Slider({
         Default = 50,
     },
     Callback = function(value)
-        KillAuraDistance = value
+        KillAuraDistance = tonumber(value)
+    end
+})
+
+local SpeedHacksSlider = PlayerTab:Slider({
+	Title = "Walk Speed",
+	Step = 1,
+
+	Value = {
+		Min = 0,
+		Max = 100,
+		Default = 16,
+	},
+	Callback = function(value)
+		speed = value
+	end
+})
+
+local SpeedHacksToggle = PlayerTab:Toggle({
+    Title = "Enable Speed Hacks",
+    Callback = function(state)
+        if state then
+			speedConnection = runservice.RenderStepped:Connect(function()
+				hum.WalkSpeed = speed
+			end)
+		else
+			speedConnection:Disconnect()
+			speedConnection = nil
+			hum.WalkSpeed = 16
+		end
+    end
+})
+
+local JumpHacksSlider = PlayerTab:Slider({
+	Title = "Jump Power",
+	Step = 1,
+
+	Value = {
+		Min = 0,
+		Max = 100,
+		Default = 50,
+	},
+	Callback = function(value)
+		jump = value
+	end
+})
+
+local JumpHacksToggle = PlayerTab:Toggle({
+    Title = "Enable Jump Hacks",
+    Callback = function(state)
+        if state then
+			jumpConnection = runservice.RenderStepped:Connect(function()
+				hum.JumpPower = jump
+			end)
+		else
+			jumpConnection:Disconnect()
+			jumpConnection = nil
+			hum.JumpPower = 50
+		end
+    end
+})
+
+local InfJumpToggle = PlayerTab:Toggle({
+    Title = "Infinite Jump",
+    Callback = function(state)
+        if state then
+            infJumpConnection = uis.JumpRequest:Connect(function()
+                hum:ChangeState(Enum.HumanoidStateType.Jumping)
+            end)
+        else
+            infJumpConnection:Disconnect() end
+            infJumpConnection = nil
+        end
     end
 })
 
